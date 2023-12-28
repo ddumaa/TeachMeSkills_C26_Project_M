2d02765b7bf826195a6792422d45059393ae8659
MarkSlepter <131391955+MarkSlepter@users.noreply.github.com>
2023-12-28 14:21:11 +0100

    Second commit

--- src/course_project/services/authorization/Authorization.java
+++
@@ -1,4 +0,0 @@
-package course_project.services.authorization;
-
-public class Authorization {
-}
---
+++ src/course_project/storage/StorageMock.java
@@ -0,0 +1,16 @@
+package course_project.storage;
+
+public class StorageMock {
+
+    private final String login = "user";
+    private final String password = "changeme123";
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
