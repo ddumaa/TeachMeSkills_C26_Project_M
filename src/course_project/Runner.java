886abbbaf919aa9fa2b175a91a59da0c7da0eff4
ddumaa <dimaduma62@gmail.com>
2024-01-04 01:35:02 +0300

    fix

--- src/course_project/Runner.java
+++ src/course_project/Runner.java
@@ -4,7 +4,6 @@ import course_project.packages.logger.Logging;
 import course_project.services.reading_writing_files.ReadWritFiles;
 
 import java.util.Date;
-import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
