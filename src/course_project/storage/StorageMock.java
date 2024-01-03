cad930b5c511540d9a68b43ef01f5925e236abb2
MarkSlepter <131391955+MarkSlepter@users.noreply.github.com>
2024-01-03 19:52:46 +0100

    Fourth commit

--- src/course_project/storage/StorageMock.java
+++ src/course_project/storage/StorageMock.java
@@ -2,8 +2,8 @@ package course_project.storage;
 
 public class StorageMock {
 
-    private final String login = "5qk7jhrnladXNlcg=="; //encrypted with Base64 and salt. Original: user
-    private final String password = "sjenzraa77Y2hhbmdlbWUxMjM="; //encrypted with Base64 and salt. Original: changeme123
+    private final String login = "bpwxo25108dXNlcg==bpwxo25108"; //encrypted with Base64 and salt. Original: user
+    private final String password = "7fsf7l7oa5Y2hhbmdlbWUxMjM=7fsf7l7oa5"; //encrypted with Base64 and salt. Original: changeme123
 
     public String getLogin() {
         return login;
