b15c067b4abb4af23cf408ef0cdcf37db47d327e
MarkSlepter <131391955+MarkSlepter@users.noreply.github.com>
2024-01-08 16:42:45 +0100

    Refinement of classes

--- src/course_project/services/coder/Coder.java
+++ src/course_project/services/coder/Coder.java
@@ -7,7 +7,9 @@ import java.util.Arrays;
 
 public class Coder {
 
+    //Method to encrypt sensitive user information using Base64 class
     public static String code(String input){
+
 
         String encodedString = Base64.getEncoder().encodeToString(input.getBytes());
         String result = addSalt(encodedString);
@@ -15,20 +17,27 @@ public class Coder {
 
     }
 
+    //Method to decrypt sensitive user information using Base64 class
     public static String decode(String input){
 
         byte[] decodedBytes = Base64.getDecoder().decode(input);
+
         if (decodedBytes.length >= 20){
+
             decodedBytes = Arrays.copyOfRange(decodedBytes, 10, decodedBytes.length - 10);
+
         } else {
-            System.out.println("недостаточно байтов для корректного выделения подстроки.");
-            //return "";
+
+            System.out.println("The entered data cannot be encoded due to insufficient bytes.");
+
         }
+
         String decoded = new String(decodedBytes);
         return decoded;
 
     }
 
+    //A method that enhances the security of encrypted sensitive user information
     private static String addSalt(String input){
 
         String symbols = "abcdefghijklmnopqrstuvwxyz0123456789";
--- src/course_project/services/session/Session.java
+++ src/course_project/services/session/Session.java
@@ -5,44 +5,104 @@ import java.util.Date;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import static course_project.packages.logger.Logging.EXECUTION_LOG;
+
 public class Session {
 
     private String accessToken;
     private Date expDate;
+    private int loginAttempts;
 
+    //Method that initializes the start of a session
     public Session() {
+
         setAccessToken();
-        this.expDate = expDate;
+        setExpDate();
+        this.loginAttempts = 0;
+
+        EXECUTION_LOG(new Date(), "Session started. Access Token: " + accessToken);
 
     }
 
-    public boolean isSessionAlive(){
-
-        if (this.accessToken.length() == 16 &&
-                this.expDate.after(new Date())){
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-    private void setAccessToken(){
-
+    //Method that generates an access token
+    private void setAccessToken() {
         String symbols = "abcdefghijklmnopqrstuvwxyz0123456789";
-        this.accessToken = new Random().ints(10, 0, symbols.length())
+        this.accessToken = new Random().ints(16, 0, symbols.length())
                 .mapToObj(symbols::charAt)
                 .map(Object::toString)
                 .collect(Collectors.joining());
-
     }
 
-    private void setExpDate(){
-
+    //Method that sets the session timer
+    private void setExpDate() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         calendar.add(Calendar.MINUTE, 10);
         this.expDate = calendar.getTime();
+
+    }
+
+    //Method for checking active session
+    private boolean isSessionAlive() {
+
+        if (this.accessToken.length() == 16 &&
+                this.expDate.after(new Date())) {
+
+            return true;
+
+        } else {
+
+            EXECUTION_LOG(new Date(), "Session ended.");
+            return false;
+
+        }
+
+    }
+
+    //Method that counts the number of password attempts
+    private void attemptLogin(String password) {
+
+        if (checkPassword(password)) {
+
+            EXECUTION_LOG(new Date(), "Password entered successfully. Access Token: " + accessToken);
+
+        } else {
+
+            loginAttempts++;
+            EXECUTION_LOG(new Date(), "Incorrect password entered." + accessToken);
+
+            if(loginAttempts >= 3) {
+
+                exitSystem();
+
+            }
+
+        }
+
+    }
+
+    //A method that interrupts the program if there are more than one password entry attempt.
+    private void exitSystem(){
+
+        EXECUTION_LOG(new Date(), "The number of password attempts has been exceeded. Stopping the program.");
+            System.exit(0);
+
+    }
+
+    //A method that checks whether a password is entered correctly
+    private boolean checkPassword(String password) {
+
+        if (password.matches(".*\\d.*") && password.length() >= 8 && "changeme123".equals(password)){
+
+            EXECUTION_LOG(new Date(), "Password verification passed." + accessToken);
+            return true;
+
+        } else {
+
+            EXECUTION_LOG(new Date(), "Password verification failed." + accessToken);
+            return false;
+
+        }
 
     }
 
